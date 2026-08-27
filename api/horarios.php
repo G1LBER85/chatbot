d43914dcf5da8160ca5a033c0743c9f4110ed383
Gilber <gilber@bdino.app>
2026-08-27 11:33:09 -0600

    hora lista

--- api/horarios.php
+++ api/horarios.php
@@ -3,8 +3,8 @@ header('Content-Type: application/json; charset=utf-8');
 // Horarios permitidos del plantel
 
 $HORARIOS = [
-    'entrada' => ['inicio' => 18,  'fin' => 20],
-    'salida'  => ['inicio' => 21, 'fin' => 23]
+    'entrada' => ['inicio' => 6, 'fin' => 12],
+    'salida'  => ['inicio' => 12, 'fin' => 15]
 ];
 
 echo json_encode($HORARIOS, JSON_UNESCAPED_UNICODE);
