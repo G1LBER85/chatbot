04c92bd1b9a84234b37e9585763d7d616a56d95b
César Escobar <escobarmoralescesaralejandro@gmail.com>
2026-08-30 22:13:36 -0600

    Rediseño de registro.html

--- api/ultimo-registro.php
+++ api/ultimo-registro.php
@@ -8,8 +8,8 @@ $sql = "
         a.grado,
         a.foto,
         r.tipo,
-        DATE_FORMAT(r.fecha_hora, '%h:%i %p')        AS hora,
-        DATE_FORMAT(r.fecha_hora, '%e de %M de %Y')  AS fecha
+        DATE_FORMAT(r.fecha_hora, '%h:%i %p |')        AS hora,
+        DATE_FORMAT(r.fecha_hora, '%e de %M del %Y')  AS fecha
     FROM registros r
     JOIN alumnos a ON a.id = r.alumno_id
     ORDER BY r.id DESC
